42d6052e4bc200b5125e55a48e6471ee63714d0f
bartekg26 <bxaxrx12@wp.pl>
2016-03-31 09:43:49 +0200

    piąty commit

--- ConsoleApplication1 hello/Program.cs
+++ ConsoleApplication1 hello/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1_hello
         static void Main(string[] args)
         {
 
-            Console.WriteLine("czwarty commit");
+            Console.WriteLine("piaty commit");
 
 
         }
