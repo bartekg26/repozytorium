4c37353c35bf741829f83defd00c57ca62aa9f23
bartekg26 <bxaxrx12@wp.pl>
2016-03-31 09:42:19 +0200

    drugi commit

--- ConsoleApplication1 hello/Program.cs
+++ ConsoleApplication1 hello/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1_hello
         static void Main(string[] args)
         {
 
-            Console.WriteLine("oks7777yyyy");
+            Console.WriteLine("drugi commit");
 
 
         }
